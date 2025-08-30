e1609c50fffd0f00c37497b3a218851512cbecb2
Andre Menezes <djdedemenezes@gmail.com>
2025-08-30 14:26:20 -0300

    add volunteer user stories

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -2,4 +2,27 @@ class VolunteersController < ApplicationController
   def index
     @volunteers = Volunteer.all
   end
+
+  def show
+    @volunteer = Volunteer.find(params[:id])
+  end
+
+  def new
+    @volunteer = Volunteer.new
+  end
+
+  def create
+    @volunteer = Volunteer.new(volunteer_params)
+    if @volunteer.save
+      redirect_to volunteer_path(@volunteer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def volunteer_params
+    params.require(:volunteer).permit(:name)
+  end
 end
